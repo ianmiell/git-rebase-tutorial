1419e272790cf7286a75bf2e141c380015bb2259
Ian Miell <ian.miell@gmail.com>
2018-03-29 13:36:22 +0100

    updaes

--- git_rebase_tutorial.py
+++ git_rebase_tutorial.py
@@ -51,13 +51,13 @@ CTRL-] (right angle bracket) to continue.
 		)
 		shutit.challenge(
 			'Create a file called "afile" with the line "Line1" in it, add and commit it to the git repository.',
-			'b3d7557d0111e9f609ccc0a9697cf6cd',
+			'5596bf0e157e6d7d7f0a5034e8609c13',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['"echo Line1 > afile" will create the file.','add','"git add afile" to add the file','commit',"""'git commit -am "adding afile"' to commit the file"""],
 			congratulations='OK, file create and committed!',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_5',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_8'
@@ -66,13 +66,13 @@ CTRL-] (right angle bracket) to continue.
 		self._show_graph(shutit)
 		shutit.challenge(
 			'Add line "Line2" to file "afile", and commit it to the git repository.',
-			'ad16df00e2cbe96ca966924ee9e522fd',
+			'6657ee19dcb40c5dfcdbf91f1e45a918',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['"echo Line2 >> afile" will add the line to the file','"git commit -am line2added" to commit the file'],
 			congratulations='OK! Line2 added and committed.',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_8',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_10'
@@ -96,13 +96,13 @@ CTRL-] (right angle bracket) to continue.
 		self._show_graph(shutit)
 		shutit.challenge(
 			'Continuing on master, add a Line3 to "afile" and commit it',
-			'fd43955a1b0b8ef6231f9cf0370a07c9',
+			'd948490d6683d647c3619c9082eac4e0',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['echo Line3 >> afile, then git commit -am Line3added'],
 			congratulations='OK! Line3 added and committed.',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_11',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_13'
@@ -126,13 +126,13 @@ CTRL-] (right angle bracket) to continue.
 		self._show_graph(shutit)
 		shutit.challenge(
 			'Add a line "FeatureLine1" to "afile" on this branch and commit it',
-			'c049eb2a7f7f8a2fb8b3753f417f6aeb',
+			'1984e7dfbc27dfe90401a15c8b969a27',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=[],
 			congratulations='OK! Line added and committed.',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_14',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_16'
@@ -141,13 +141,13 @@ CTRL-] (right angle bracket) to continue.
 		self._show_graph(shutit)
 		shutit.challenge(
 			'Check out master, and merge feature_1 into it.\nResolve the conflict, placing FeatureLine1 after Line3, and commit',
-			'f950d22727f62b4c9c1a80624291e11f',
+			'6caf372d723434c8ca900b2bfb9f03e5',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['git checkout master, then git merge feature_1'],
 			congratulations='OK! Merge done correctly.',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_16',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_20'
@@ -155,13 +155,13 @@ CTRL-] (right angle bracket) to continue.
 		)
 		shutit.challenge(
 			'Now have a look at the history graph with "git log --graph --decorate --oneline", and observe that it is quite messy.',
-			'f950d22727f62b4c9c1a80624291e11f',
+			'6caf372d723434c8ca900b2bfb9f03e5',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['git log --graph --decorate --oneline'],
 			congratulations='OK! You might want to run the same command in future to see the state of the git repository.',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_20',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_20'
@@ -170,13 +170,13 @@ CTRL-] (right angle bracket) to continue.
 		# TODO: list the other two ways
 		shutit.challenge(
 			'We are going to back out this merge in the git history.\nCheck out the commit before FeatureLine1 was added. This is a trickier one, there are at least three ways to achieve this.',
-			'fd43955a1b0b8ef6231f9cf0370a07c9',
+			'6657ee19dcb40c5dfcdbf91f1e45a918',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['git log to see the history','git checkout <ID>','git checkout HEAD^'],
 			congratulations='OK! Parent commit checked out. The three ways were:\n\tgit checkout HEAD^\n\tgit checkout <commit id>\n\tgit checkout HEAD~1',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_20',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_21'
@@ -185,13 +185,13 @@ CTRL-] (right angle bracket) to continue.
 		self._show_graph(shutit)
 		shutit.challenge(
 			'Force the master branch to be pointed to the point in the history you have just checked out.\n\nOnce that is done, check out the just-moved master branch.\n\nAll you are doing here is reverting your git tree to a previous point so that we can have a cleaner git history. This is not the rebase step! If you are confused here, just use the help (CTRL-h) as it is not important to the tutorial.',
-			'1c85c8de900bc974f22976a4163b23cb',
+			'3b587ce6aaf50a10a82b983d194f22f8',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['You might want to try git branching with the already-used branch name. But you might need to be forceful.','git branch -f master && git checkout master'],
 			congratulations='OK! Master branch moved.',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *) <(git branch | wc -l)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) <(git branch | wc -l) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_21',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_22'
@@ -200,13 +200,13 @@ CTRL-] (right angle bracket) to continue.
 		self._show_graph(shutit)
 		shutit.challenge(
 			'Check out the feature_1 branch, and rebase master against it',
-			'f950d22727f62b4c9c1a80624291e11f',
+			'aba6936fbcef02637df5f7b07fb8084e',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['git checkout feature_1, then git rebase master'],
 			congratulations='OK! master re-based against feature_1',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) <(git branch | wc -l) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_22',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_27'
@@ -215,13 +215,13 @@ CTRL-] (right angle bracket) to continue.
 		self._show_graph(shutit)
 		shutit.challenge(
 			'Check out master and merge feature_1 against it',
-			'f950d22727f62b4c9c1a80624291e11f',
+			'aba6936fbcef02637df5f7b07fb8084e',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['git checkout master, then git merge feature_1'],
 			congratulations='OK! Master merged with feature_1',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) <(git branch | wc -l) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_27',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_29'
@@ -229,14 +229,14 @@ CTRL-] (right angle bracket) to continue.
 		)
 		self._show_graph(shutit)
 		shutit.challenge(
-			'Now have a look at the history graph with "git log --graph --decorate --oneline", comparing it with before. It is a lot cleaner!',
-			'f950d22727f62b4c9c1a80624291e11f',
+			'Now have a look at the history graph with "git log --graph --decorate --oneline", comparing it with before. It is a lot cleaner than it was before!',
+			'aba6936fbcef02637df5f7b07fb8084e',
 			challenge_type='golf',
 			expect_type='md5sum',
 			hints=['git log --graph --decorate --oneline'],
 			congratulations='OK!',
 			follow_on_context={
-				'check_command':'cat afile <(git status -s) <(find *)',
+				'check_command':r"echo $(cat afile <(git status -s) <(find *) <(git branch | wc -l) | tr -d '\n')",
 				'context':'docker',
 				'reset_container_name':'imiell/git-rebase-tutorial:step_29',
 				'ok_container_name':'imiell/git-rebase-tutorial:step_29'
